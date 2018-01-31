56e9062778b054f74a1bb6a0c71abfad6483c388
tinkrmind <tinkrmind@gmail.com>
2018-01-31 18:45:50 -0500

    Chnaged the voice

--- fuckYou.py
+++ fuckYou.py
@@ -5,8 +5,8 @@ shell = True
 
 def speak(this):
     # print(this)
-    this = this.replace("-", "from")
+    this = this.replace("-", ". From; ")
     # print(this)
-    subprocess.run(['flite', '-t', this])
+    subprocess.run(['flite', '-voice', 'file://cmu_us_aew.flitevox', '-t', str(this)])
 
 speak(str(fuck.random(from_='tinkermind').text))
